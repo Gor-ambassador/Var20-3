ab21a0b07d7c9712e2f4d4d762a984192e626d7d
Gor Saribekyan <78426886+Gor-ambassador@users.noreply.github.com>
2023-03-10 23:21:37 +0300

    Add files via upload

---
+++ Hash/DBStructure/Info.cs
@@ -0,0 +1,233 @@
+﻿/*
+ Массив данных о пассажирах теплохода:
+ * ФИО пассажира,
+ * номер каюты,
+ * тип каюты (люкс, 1, 2, 3 классы),
+ * порт назначения (сравнение по полям – номер каюты, порт назначения, ФИО)
+ */
+
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash.DBStructure
+{
+    /// <summary>
+    /// Class with data about passenger
+    /// </summary>
+    public class BaseInfo
+    {
+
+        /// <value>
+        /// Service field for interaction with the database
+        /// </value>
+        [Key]
+        public int InfoId { get; set; }
+        /// <value>
+        /// Passenger name
+        /// </value>
+        public string Name { get; set; }
+        /// <value>
+        /// Destination port
+        /// </value>
+        public string Port { get; set; }
+        /// <value>
+        /// Passenger room number
+        /// </value>
+        public int RoomNumber { get; set; }
+        /// <value>
+        /// Passenger room type
+        /// </value>
+        public string RoomType { get; set; }
+
+        public BaseInfo(int InfoId = 0, string Name = "", string Port = "", int RoomNumber = 0, string RoomType = "") {
+            this.InfoId = InfoId;
+            this.Name = Name;
+            this.Port = Port;
+            this.RoomNumber = RoomNumber;
+            this.RoomType = RoomType;
+        }
+
+        public BaseInfo(BaseInfo bi)
+        {
+            this.InfoId = bi.InfoId;
+            this.Name = bi.Name;
+            this.Port = bi.Port;
+            this.RoomNumber = bi.RoomNumber;
+            this.RoomType = bi.RoomType;
+        }
+
+        /// <summary>
+        /// Operator < overloading
+        /// </summary>
+        /// <returns>
+        /// True if left element is less to the right, else false
+        /// </returns>
+        /// <param name="left">
+        /// Left element
+        /// </param>
+        /// /// <param name="right">
+        /// Right element
+        /// </param>
+        public static bool operator <(BaseInfo left, BaseInfo right)
+        {
+            if (left.RoomNumber == right.RoomNumber)
+            {
+                if (left.Port == right.Port)
+                {
+                    if (left.Name == right.Name)
+                        return false;
+                    else if (string.Compare(left.Port, right.Port) < 0)
+                        return true;
+                    return false;
+                }
+                else if (string.Compare(left.Port, right.Port) < 0)
+                    return true;
+                return false;
+            }
+            else if (left.RoomNumber < right.RoomNumber)
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Operator > overloading
+        /// </summary>
+        /// <returns>
+        /// True if left element is greater to the right, else false
+        /// </returns>
+        /// <param name="left">
+        /// Left element
+        /// </param>
+        /// /// <param name="right">
+        /// Right element
+        /// </param>
+        public static bool operator >(BaseInfo left, BaseInfo right)
+        {
+            if (left.RoomNumber == right.RoomNumber)
+            {
+                if (left.Port == right.Port)
+                {
+                    if (left.Name == right.Name)
+                        return false;
+                    else if (string.Compare(left.Name, right.Name) > 0)
+                        return true;
+                    return false;
+                }
+                else if (string.Compare(left.Port, right.Port) > 0)
+                    return true;
+                return false;
+            }
+            else if (left.RoomNumber > right.RoomNumber)
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Operator <= overloading
+        /// </summary>
+        /// <returns>
+        /// True if left element is less or equal to the right, else false
+        /// </returns>
+        /// <param name="left">
+        /// Left element
+        /// </param>
+        /// /// <param name="right">
+        /// Right element
+        /// </param>
+        public static bool operator <=(BaseInfo left, BaseInfo right)
+        {
+
+            if (left.RoomNumber == right.RoomNumber)
+            {
+                if (left.Port == right.Port)
+                {
+                    if (left.Name == right.Name)
+                        return true;
+                    else if (string.Compare(left.Name, right.Name) < 0)
+                        return true;
+                    return false;
+                }
+                else if (string.Compare(left.Port, right.Port) < 0)
+                    return true;
+                return false;
+            }
+            else if (left.RoomNumber < right.RoomNumber)
+                return true;
+
+            return false;
+
+        }
+
+        /// <summary>
+        /// Operator >= overloading
+        /// </summary>
+        /// <returns>
+        /// True if left element is greater or equal to the right, else false
+        /// </returns>
+        /// <param name="left">
+        /// Left element
+        /// </param>
+        /// /// <param name="right">
+        /// Right element
+        /// </param>
+        public static bool operator >=(BaseInfo left, BaseInfo right)
+        {
+
+            if (left.RoomNumber == right.RoomNumber)
+            {
+                if (left.Port == right.Port)
+                {
+                    if (left.Name == right.Name)
+                        return true;
+                    else if (string.Compare(left.Name, right.Name) > 0)
+                        return true;
+                    return false;
+                }
+                else if (string.Compare(left.Port, right.Port) > 0)
+                    return true;
+                return false;
+            }
+            else if (left.RoomNumber > right.RoomNumber)
+                return true;
+
+            return false;
+
+            //return String.Compare(left.Port, right.Port) >= 0;
+        }
+    }
+
+    public class InfoGood : BaseInfo
+    {
+        public UInt32 Hash;
+
+        public InfoGood(int InfoId = 0, string Name = "", string Port = "", int RoomNumber = 0, string RoomType = "")
+            : base(InfoId, Name, Port, RoomNumber, RoomType)
+        { Hash = Program.GoodHash(Port); }
+
+        public InfoGood(BaseInfo info) : base(info)
+        { Hash = Program.GoodHash(Port); }
+    }
+
+    public class InfoBad : BaseInfo
+    {
+        public UInt32 Hash;
+
+        public InfoBad(int InfoId = 0, string Name = "", string Port = "", int RoomNumber = 0, string RoomType = "")
+            : base(InfoId, Name, Port, RoomNumber, RoomType)
+
+        { Hash = Program.BadHash(Port); }
+   
+
+        public InfoBad(BaseInfo info) : base(info)
+        { Hash = Program.BadHash(Port); }
+    }
+}
---
+++ Hash/Program.cs
@@ -0,0 +1,414 @@
+﻿using Hash.DBStructure;
+using System.Text;
+
+namespace Hash
+{
+    internal class Program
+    {
+        /// <summary>Bad hash function -  43283 (14%) collisions on 300000 elements</summary>
+        /// <param name="input">Input string</param>
+        /// <returns>UInt32 hash value</returns>
+        public static UInt32 BadHash(string input)
+        {
+            UInt32 result = 0;
+
+            for(UInt32 i = 0; i < input.Length; i += 2)
+                result += (Convert.ToUInt32(input[Convert.ToInt32(i)]) << Convert.ToInt32(i % 12)) ^ (Convert.ToUInt32(input[Convert.ToInt32(i)]));
+            
+            for (UInt32 i = 1; i < input.Length; i += 2)
+                result += (Convert.ToUInt32(input[Convert.ToInt32(i)]) >> Convert.ToInt32(i % 12)) ^ (Convert.ToUInt32(input[Convert.ToInt32(i)]));
+
+            return result;
+        }
+
+        /// <summary>Good hash function - 67 (0.0002%) collisions on 300000 elements</summary>
+        /// <param name="input">Input string</param>
+        /// <returns>UInt32 hash value</returns>
+        public static UInt32 GoodHash(string input)
+        {
+            UInt32 result = 0;
+
+            for (UInt32 i = 0; i < input.Length; i += 2)
+                result += (Convert.ToUInt32(input[Convert.ToInt32(i)]) << Convert.ToInt32(i % 23)) ^ (Convert.ToUInt32(input[Convert.ToInt32(i)]));
+
+            for (UInt32 i = 1; i < input.Length; i += 2)
+                result += (Convert.ToUInt32(input[Convert.ToInt32(i)]) >> Convert.ToInt32(i % 23)) ^ (Convert.ToUInt32(input[Convert.ToInt32(i)]));
+
+            return result;
+        }
+
+        /// <summary>Function that searches the element by GoodHash in LookUp</summary>
+        /// <param name="data">Input LookUp</param>
+        /// <param name="find">Element to find</param>
+        /// <returns>Element found</returns>
+        static InfoGood SearchElement(Lookup<UInt32, InfoGood> data, InfoGood find)
+        {
+            return data.Where(x => x.Key == find.Hash).SelectMany(x => x).First(x => x == find);
+        }
+
+        /// <summary>Function that searches the element by BadHash in LookUp</summary>
+        /// <param name="data">Input LookUp</param>
+        /// <param name="find">Element to find</param>
+        /// <returns>Element found</returns>
+        static InfoBad SearchElement(Lookup<UInt32, InfoBad> data, InfoBad find)
+        {
+            return data.Where(x => x.Key == find.Hash).SelectMany(x => x).First(x => x == find);
+        }
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+           using (var db = new DataContext100())
+            {
+                List<BaseInfo> infos = db.Info.ToList();
+                List<InfoGood> infosGood = infos.Select(x => new InfoGood(x)).ToList();
+                List<InfoBad> infosBad = infos.Select(x => new InfoBad(x)).ToList();
+
+                Lookup<UInt32, InfoGood> infosGoodLook = (Lookup<UInt32, InfoGood>)infosGood.ToLookup(x => x.Hash, x => x);
+                Lookup<UInt32, InfoBad> infosBadLook = (Lookup<UInt32, InfoBad>)infosBad.ToLookup(x => x.Hash, x => x);
+
+                InfoGood findGood = infosGood[random.Next(infosGood.Count)];
+
+                DateTime start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                findGood = infosGood[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                DateTime end = DateTime.Now;
+
+                Console.WriteLine($"Good Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element");
+
+                InfoBad findBad = infosBad[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosBadLook, findBad);
+
+                end = DateTime.Now;
+
+                Console.WriteLine($"Bad Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element \n");
+
+                Console.WriteLine($"GoodHash, {infos.Count} elements, {infosGood.Count - infosGoodLook.Count} collisions");
+                Console.WriteLine($"BadHash, {infos.Count} elements, {infosBad.Count - infosBadLook.Count} collisions \n\n");
+            };
+     
+            using (var db = new DataContext1000())
+            {
+                List<BaseInfo> infos = db.Info.ToList();
+                List<InfoGood> infosGood = infos.Select(x => new InfoGood(x)).ToList();
+                List<InfoBad> infosBad = infos.Select(x => new InfoBad(x)).ToList();
+
+                Lookup<UInt32, InfoGood> infosGoodLook = (Lookup<UInt32, InfoGood>)infosGood.ToLookup(x => x.Hash, x => x);
+                Lookup<UInt32, InfoBad> infosBadLook = (Lookup<UInt32, InfoBad>)infosBad.ToLookup(x => x.Hash, x => x);
+
+                InfoGood findGood = infosGood[random.Next(infosGood.Count)];
+
+                DateTime start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                findGood = infosGood[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                DateTime end = DateTime.Now;
+
+                Console.WriteLine($"Good Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element");
+
+                InfoBad findBad = infosBad[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosBadLook, findBad);
+
+                end = DateTime.Now;
+
+                Console.WriteLine($"Bad Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element \n");
+
+                Console.WriteLine($"GoodHash, {infos.Count} elements, {infosGood.Count - infosGoodLook.Count} collisions");
+                Console.WriteLine($"BadHash, {infos.Count} elements, {infosBad.Count - infosBadLook.Count} collisions \n \n");
+            };
+
+            using (var db = new DataContext10000())
+            {
+                List<BaseInfo> infos = db.Info.ToList();
+                List<InfoGood> infosGood = infos.Select(x => new InfoGood(x)).ToList();
+                List<InfoBad> infosBad = infos.Select(x => new InfoBad(x)).ToList();
+
+                Lookup<UInt32, InfoGood> infosGoodLook = (Lookup<UInt32, InfoGood>)infosGood.ToLookup(x => x.Hash, x => x);
+                Lookup<UInt32, InfoBad> infosBadLook = (Lookup<UInt32, InfoBad>)infosBad.ToLookup(x => x.Hash, x => x);
+
+                InfoGood findGood = infosGood[random.Next(infosGood.Count)];
+
+                DateTime start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                findGood = infosGood[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                DateTime end = DateTime.Now;
+
+                Console.WriteLine($"Good Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element");
+
+                InfoBad findBad = infosBad[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosBadLook, findBad);
+
+                end = DateTime.Now;
+
+                Console.WriteLine($"Bad Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element \n");
+
+                Console.WriteLine($"GoodHash, {infos.Count} elements, {infosGood.Count - infosGoodLook.Count} collisions");
+                Console.WriteLine($"BadHash, {infos.Count} elements, {infosBad.Count - infosBadLook.Count} collisions \n \n");
+            };
+
+            using (var db = new DataContext20000())
+            {
+                List<BaseInfo> infos = db.Info.ToList();
+                List<InfoGood> infosGood = infos.Select(x => new InfoGood(x)).ToList();
+                List<InfoBad> infosBad = infos.Select(x => new InfoBad(x)).ToList();
+
+                Lookup<UInt32, InfoGood> infosGoodLook = (Lookup<UInt32, InfoGood>)infosGood.ToLookup(x => x.Hash, x => x);
+                Lookup<UInt32, InfoBad> infosBadLook = (Lookup<UInt32, InfoBad>)infosBad.ToLookup(x => x.Hash, x => x);
+
+                InfoGood findGood = infosGood[random.Next(infosGood.Count)];
+
+                DateTime start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                findGood = infosGood[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                DateTime end = DateTime.Now;
+
+                Console.WriteLine($"Good Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element");
+
+                InfoBad findBad = infosBad[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosBadLook, findBad);
+
+                end = DateTime.Now;
+
+                Console.WriteLine($"Bad Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element \n");
+
+                Console.WriteLine($"GoodHash, {infos.Count} elements, {infosGood.Count - infosGoodLook.Count} collisions");
+                Console.WriteLine($"BadHash, {infos.Count} elements, {infosBad.Count - infosBadLook.Count} collisions \n \n");
+            };
+
+            using (var db = new DataContext40000())
+            {
+                List<BaseInfo> infos = db.Info.ToList();
+                List<InfoGood> infosGood = infos.Select(x => new InfoGood(x)).ToList();
+                List<InfoBad> infosBad = infos.Select(x => new InfoBad(x)).ToList();
+
+                Lookup<UInt32, InfoGood> infosGoodLook = (Lookup<UInt32, InfoGood>)infosGood.ToLookup(x => x.Hash, x => x);
+                Lookup<UInt32, InfoBad> infosBadLook = (Lookup<UInt32, InfoBad>)infosBad.ToLookup(x => x.Hash, x => x);
+
+                InfoGood findGood = infosGood[random.Next(infosGood.Count)];
+
+                DateTime start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                findGood = infosGood[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                DateTime end = DateTime.Now;
+
+                Console.WriteLine($"Good Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element");
+
+                InfoBad findBad = infosBad[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosBadLook, findBad);
+
+                end = DateTime.Now;
+
+                Console.WriteLine($"Bad Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element \n");
+
+                Console.WriteLine($"GoodHash, {infos.Count} elements, {infosGood.Count - infosGoodLook.Count} collisions");
+                Console.WriteLine($"BadHash, {infos.Count} elements, {infosBad.Count - infosBadLook.Count} collisions \n \n");
+            };
+
+            using (var db = new DataContext60000())
+            {
+                List<BaseInfo> infos = db.Info.ToList();
+                List<InfoGood> infosGood = infos.Select(x => new InfoGood(x)).ToList();
+                List<InfoBad> infosBad = infos.Select(x => new InfoBad(x)).ToList();
+
+                Lookup<UInt32, InfoGood> infosGoodLook = (Lookup<UInt32, InfoGood>)infosGood.ToLookup(x => x.Hash, x => x);
+                Lookup<UInt32, InfoBad> infosBadLook = (Lookup<UInt32, InfoBad>)infosBad.ToLookup(x => x.Hash, x => x);
+
+                InfoGood findGood = infosGood[random.Next(infosGood.Count)];
+
+                DateTime start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                findGood = infosGood[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                DateTime end = DateTime.Now;
+
+                Console.WriteLine($"Good Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element");
+
+                InfoBad findBad = infosBad[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosBadLook, findBad);
+
+                end = DateTime.Now;
+
+                Console.WriteLine($"Bad Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element \n");
+
+                Console.WriteLine($"GoodHash, {infos.Count} elements, {infosGood.Count - infosGoodLook.Count} collisions");
+                Console.WriteLine($"BadHash, {infos.Count} elements, {infosBad.Count - infosBadLook.Count} collisions \n \n");
+            };
+
+            using (var db = new DataContext80000())
+            {
+                List<BaseInfo> infos = db.Info.ToList();
+                List<InfoGood> infosGood = infos.Select(x => new InfoGood(x)).ToList();
+                List<InfoBad> infosBad = infos.Select(x => new InfoBad(x)).ToList();
+
+                Lookup<UInt32, InfoGood> infosGoodLook = (Lookup<UInt32, InfoGood>)infosGood.ToLookup(x => x.Hash, x => x);
+                Lookup<UInt32, InfoBad> infosBadLook = (Lookup<UInt32, InfoBad>)infosBad.ToLookup(x => x.Hash, x => x);
+
+                InfoGood findGood = infosGood[random.Next(infosGood.Count)];
+
+                DateTime start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                findGood = infosGood[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                DateTime end = DateTime.Now;
+
+                Console.WriteLine($"Good Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element");
+
+                InfoBad findBad = infosBad[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosBadLook, findBad);
+
+                end = DateTime.Now;
+
+                Console.WriteLine($"Bad Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element \n");
+
+                Console.WriteLine($"GoodHash, {infos.Count} elements, {infosGood.Count - infosGoodLook.Count} collisions");
+                Console.WriteLine($"BadHash, {infos.Count} elements, {infosBad.Count - infosBadLook.Count} collisions \n \n");
+            };
+
+            using (var db = new DataContext100000())
+            {
+                List<BaseInfo> infos = db.Info.ToList();
+                List<InfoGood> infosGood = infos.Select(x => new InfoGood(x)).ToList();
+                List<InfoBad> infosBad = infos.Select(x => new InfoBad(x)).ToList();
+
+                Lookup<UInt32, InfoGood> infosGoodLook = (Lookup<UInt32, InfoGood>)infosGood.ToLookup(x => x.Hash, x => x);
+                Lookup<UInt32, InfoBad> infosBadLook = (Lookup<UInt32, InfoBad>)infosBad.ToLookup(x => x.Hash, x => x);
+
+                InfoGood findGood = infosGood[random.Next(infosGood.Count)];
+
+                DateTime start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                findGood = infosGood[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                DateTime end = DateTime.Now;
+
+                Console.WriteLine($"Good Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element");
+
+                InfoBad findBad = infosBad[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosBadLook, findBad);
+
+                end = DateTime.Now;
+
+                Console.WriteLine($"Bad Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element \n");
+
+                Console.WriteLine($"GoodHash, {infos.Count} elements, {infosGood.Count - infosGoodLook.Count} collisions");
+                Console.WriteLine($"BadHash, {infos.Count} elements, {infosBad.Count - infosBadLook.Count} collisions \n \n");
+            };
+
+            using (var db = new DataContext300000())
+            {
+                List<BaseInfo> infos = db.Info.ToList();
+                List<InfoGood> infosGood = infos.Select(x => new InfoGood(x)).ToList();
+                List<InfoBad> infosBad = infos.Select(x => new InfoBad(x)).ToList();
+
+                Lookup<UInt32, InfoGood> infosGoodLook = (Lookup<UInt32, InfoGood>)infosGood.ToLookup(x => x.Hash, x => x);
+                Lookup<UInt32, InfoBad> infosBadLook = (Lookup<UInt32, InfoBad>)infosBad.ToLookup(x => x.Hash, x => x);
+
+                InfoGood findGood = infosGood[random.Next(infosGood.Count)];
+
+                DateTime start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                findGood = infosGood[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosGoodLook, findGood);
+
+                DateTime end = DateTime.Now;
+
+                Console.WriteLine($"Good Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element");
+
+                InfoBad findBad = infosBad[random.Next(infosGood.Count)];
+
+                start = DateTime.Now;
+
+                SearchElement(infosBadLook, findBad);
+
+                end = DateTime.Now;
+
+                Console.WriteLine($"Bad Hash, {infos.Count} elements, {(end - start).TotalMilliseconds} time to find element \n");
+
+                Console.WriteLine($"GoodHash, {infos.Count} elements, {infosGood.Count - infosGoodLook.Count} collisions");
+                Console.WriteLine($"BadHash, {infos.Count} elements, {infosBad.Count - infosBadLook.Count} collisions \n \n");
+            };
+        }
+    }
+}
